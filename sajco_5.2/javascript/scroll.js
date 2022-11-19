32c77e67c26533482ce4ebfd156762a3035a4523
Shumaila khan <53408176+shumaila-hub@users.noreply.github.com>
2022-11-19 12:45:23 +0500

    Add files via upload

---
+++ sajco_5.2/javascript/scroll.js
@@ -0,0 +1,28 @@
+var button = document.getElementById('right');
+button.onclick = function () {
+    var container = document.getElementById('box');
+    sideScroll(container, 'right', 25, 100, 10);
+};
+var button = document.getElementById('left');
+button.onclick = function () {
+    var container = document.getElementById('box');
+    sideScroll(container, 'left', 25, 100, 10);
+};
+
+function sideScroll(element, direction, speed, distance, step) {
+    scrollAmount = 0;
+    var slideTimer = setInterval(function () {
+        if (direction == 'left') {
+            element.scrollLeft -= step;
+        }
+        else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if (scrollAmount >= distance) {
+            window.clearInterval(slideTimer);
+        }
+
+    }, speed);
+};
+
